7244258035d888cb3101b6c362001ed5057a93fa
gaikw504 <gajanan.gaikwad@kpn.com>
2017-10-27 19:03:35 +0530

    gajanan

---
+++ passion/src/test/java/com/passion/passion/PassionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.passion.passion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PassionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
